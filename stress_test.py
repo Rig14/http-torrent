197a771ad75675fa3303553ecda1e04e8e345658
Rihard Rivis <rihard.rivis@proton.me>
2025-02-21 16:47:12 +0200

    feat: made the stress test cooler - now includes killing some nodes

--- stress_test.py
+++ stress_test.py
@@ -1,3 +1,4 @@
+import logging
 import subprocess
 import os
 import signal
@@ -9,7 +10,7 @@ NODE_COUNT = 100
 # Start subprocesses in a new process group
 processes: list[subprocess.Popen] = []
 def start_subprocess(command):
-    p = subprocess.Popen(command, preexec_fn=os.setpgrp)  # Create a new process group
+    p = subprocess.Popen(command, preexec_fn=os.setpgrp, stdout=subprocess.DEVNULL)  # Create a new process group
     processes.append(p)
 
 # Cleanup function to kill all subprocesses
@@ -29,9 +30,23 @@ time.sleep(1)
 for port in range(50000, 50000 + NODE_COUNT):
     start_subprocess(["python", "main.py", str(port)])
 
+# wait for nodes to start up then kill half of them. don't kill master
+time.sleep(40)
+for i in range(1, NODE_COUNT // 2):
+    os.killpg(os.getpgid(processes[i].pid), signal.SIGTERM)
+
+
+# create 100 more nodes
+time.sleep(30)
+for port in range(50000 + NODE_COUNT + 1, 50000 + NODE_COUNT + 100):
+    start_subprocess(["python", "main.py", str(port)])
+
+
 # Keep the main process running
 try:
     while True:
         pass
 except KeyboardInterrupt:
     print("Main app interrupted. Cleaning up...")
+
+exit(0)
